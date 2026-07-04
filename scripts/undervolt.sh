6bb818ac64c55c5dd251ebd43269912a2080621e
Nykot <rodlessar@gmail.com>
2026-07-04 18:38:46 -0300

    update

--- scripts/undervolt.sh
+++ scripts/undervolt.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-# Wait 10 seconds after login
-sleep 10
-
-# Run undervolting (adjust path if needed)
-sudo python3 /home/nykot/Ryzen-5800x3d-linux-undervolting/ruv.py -c 8 -o -30
-
-# Send dunst notification
-notify-send "Undervolting applied" "All cores set to -30"
+# Carrega o módulo caso não esteja no boot
+modprobe ryzen_smu
+# Espera 1 segundo para garantir que o device está pronto
+sleep 1
+# Aplica o undervolt (ajuste o caminho para onde o ruv.py está)
+python3 /home/nykot/Ryzen-5800x3d-linux-undervolting/ruv.py -c 16 -o -30
